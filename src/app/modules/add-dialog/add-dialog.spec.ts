206eafbeab22e301ebf80ab4739414a783c0ff36
Kamal Saini <mrkamalsaini1001@gmail.com>
2026-06-17 19:08:03 +0530

    basic modules added

---
+++ src/app/modules/add-dialog/add-dialog.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddDialog } from './add-dialog';
+
+describe('AddDialog', () => {
+  let component: AddDialog;
+  let fixture: ComponentFixture<AddDialog>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddDialog],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AddDialog);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/add-dialog/add-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-dialog',
+  imports: [],
+  templateUrl: './add-dialog.html',
+  styleUrl: './add-dialog.css',
+})
+export class AddDialog {}
